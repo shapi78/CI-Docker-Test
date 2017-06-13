f1f265341c569960a8bdbd469225efb7cbd18b12
Shachar <shapi@dudamobile.com>
2017-06-14 00:57:43 +0300

    Fixing build script

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -3,8 +3,7 @@
 if [ -f "docker-compose.yml" ]; then
 
 	echo "Rebuilding Docker Compose"
-#	docker-compose build
-	docker-compose  up --build --no-deps -d product-service website
+	sudo docker-compose  up --build --no-deps -d product-service website
 else
 	echo "Cannot find docker-compose.yml file"
 	exit 1
